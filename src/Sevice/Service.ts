aeebe0ba876cb7028631fdbbb68b06e2006704f3
gamabuntat <gamabunta@tuta.io>
2021-02-17 18:19:48 +0800

    defineButton function

--- src/Sevice/Service.ts
+++ src/Sevice/Service.ts
@@ -4,17 +4,16 @@ import Model from '../Model/Model';
 type tActiveButton = 'buttonS' | 'buttonE'
 
 export default class Service extends EventEmitter {
-  private activeButton: tActiveButton
+  private activeButton: tActiveButton[]
   constructor(private m: Model) {
     super();
-    this.activeButton = 'buttonS';
+    this.activeButton = ['buttonS', 'buttonE'];
   }
 
-  setX(x: number): void {
-    this.m[this.activeButton].buttonX = x;
-  }
-
-  getX(): number {
-    return this.m[this.activeButton].buttonX;
+  defineButton(x: number): void {
+    const diff = this.activeButton.reduce((diff, b) => (
+      Math.abs(x - (this.m[b].buttonX + this.m.buttonW)) - diff
+    ), 0)
+    diff < 0 && this.activeButton.reverse();
   }
 }
