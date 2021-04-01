9aec7ea382c73b569f182e844287740c4790db34
gamabuntat <gamabunta@tuta.io>
2021-04-02 04:27:29 +0800

    fixed button view test

--- __tests__/buttonView.test.ts
+++ __tests__/buttonView.test.ts
@@ -26,38 +26,20 @@ test('correctly set shift', () => {
   expect(mock.shift).toBe(x - rectObj.x);
 });
 
-test('correctly move button', () => {
-  document.body.innerHTML = (
-    '<button id="button"width: 2px; style="left: 10px; bottom: 0"></button>'
-  );
+test('correctly calc position', () => {
   const mock = {
-    component: document.getElementById('button')!,
+    moveButton: jest.fn(),
     shift: 1,
     transformOffset: -2,
     offset: 2,
     orient: {
       isVertical: false,
       coord: 'x',
-      styleCoord: 'left',
     }
   };
   Object.setPrototypeOf(mock, ButtonView.prototype);
-  ButtonView.prototype.moveButton.call(mock, 0, 1, 0.2, 1, 44);
-  expect(mock.component.style.left).toBe('20%');
-  ButtonView.prototype.moveButton.call(mock, 100, 1, 0.2, 1, 44);
-  expect(mock.component.style.left).toBe('100%');
-  ButtonView.prototype.moveButton.call(mock, 14, 1, 0.2, 1, 44);
-  expect(parseFloat(mock.component.style.left)).toBeGreaterThan(30);
-  ButtonView.prototype.moveButton.call(mock, 21.123, 0.3, 0.2, 1, 44);
-  expect(mock.component.style.left).toBe('30%');
-  mock.orient.isVertical = true;
-  mock.orient.styleCoord = 'bottom';
-  mock.transformOffset = -1;
-  mock.offset = 0;
-  ButtonView.prototype.moveButton.call(mock, 40, 0.6, 0, 0, 100);
-  expect(mock.component.style.bottom).toBe('60%');
-  ButtonView.prototype.moveButton.call(mock, 200, 0.6, 0, 10, 100);
-  expect(mock.component.style.bottom).toBe('0%');
-  ButtonView.prototype.moveButton.call(mock, 100, 1, 0, 0, 100);
-  expect(mock.component.style.bottom).toBe('0%');
+  ButtonView.prototype.calcPosition.call(mock, -Infinity, 1, 0.2, 1, 44);
+  expect(mock.moveButton).toHaveBeenCalledWith(0.2);
+  ButtonView.prototype.calcPosition.call(mock, Infinity, 1, 0.2, 1, 44);
+  expect(mock.moveButton).toHaveBeenCalledWith(1);
 });
