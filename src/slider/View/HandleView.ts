16d6b71642be2c7bb71c77942d3207b27f492c32
gamabuntat <schoshoev@yandex.ru>
2021-08-03 21:12:57 +0800

    feat(HandleView): added calcPosition && calcShift methods

--- src/slider/View/HandleView.ts
+++ src/slider/View/HandleView.ts
@@ -1,15 +1,20 @@
-import IHandle from '../interfaces/IHandle';
-
 class HandleView {
   private styles: CSSStyleDeclaration
-  private width: number
   private offset: number
   constructor(
     private handle: HTMLElement,
   ) {
     this.styles = getComputedStyle(this.handle);
-    this.width = parseInt(this.styles.width);
     this.offset = this.getOffset();
+  }
+
+  static calcShift(
+    pointerCoord: number,
+    containerCoord: number,
+    containerWidth: number,
+    handlePosition: number,
+  ): number {
+    return (pointerCoord - containerCoord) / containerWidth - handlePosition;
   }
 
   getOffset(): number {
@@ -17,12 +22,26 @@ class HandleView {
     return matrix.e || matrix.f;
   }
 
-  calcPosition(): number {
-    return Math.random();
+  calcPosition(
+    pointerCoord: number,
+    max: number,
+    min: number,
+    containerCoord: number,
+    containerWidth: number,
+    shift: number
+  ): number {
+    return Math.min(
+      max, 
+      Math.max(
+        min,
+        (pointerCoord - containerCoord - shift + this.offset) 
+          / containerWidth
+      )
+    );
   }
 
   moveHandle(position: number): void {
-    this.handle.style.left = String(position);
+    this.handle.style.left = `${position * 100}%`;
   }
 }
 
