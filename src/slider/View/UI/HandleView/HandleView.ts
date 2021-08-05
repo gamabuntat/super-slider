386188fe867ff9107ca2c565085a93227649659b
gamabuntat <schoshoev@yandex.ru>
2021-08-06 06:04:52 +0800

    feat(HandleView): added field lastPosition and related methods

--- src/slider/View/UI/HandleView/HandleView.ts
+++ src/slider/View/UI/HandleView/HandleView.ts
@@ -3,6 +3,8 @@ import EventBinder from '../../../EventBinder/EventBinder';
 
 class HandleView extends EventBinder implements IHandleView {
   private offset: number
+  private lastPositon = 0
+
   constructor(component: HTMLElement) {
     super(component);
     this.offset = this.getOffset();
@@ -17,22 +19,28 @@ class HandleView extends EventBinder implements IHandleView {
     containerSize,
     shift
   }: ICalcPositionArgs): number {
-    return Math.min(
-      max, 
-      Math.max(
-        min,
-        (pointerCoord - containerCoord - shift + this.offset) 
-          / containerSize
-      )
-    );
+    return Math.min(max, Math.max(min,
+      (pointerCoord - containerCoord - shift + this.offset) 
+        / containerSize
+    ));
   }
 
   moveX(position: number): void {
     this.component.style.left = `${position * 100}%`;
+    this.updateLastPosition(position);
   }
 
   moveY(position: number): void {
     this.component.style.top = `${position * 100}%`;
+    this.updateLastPosition(position);
+  }
+
+  getLastPosition(): number {
+    return this.lastPositon;
+  }
+
+  private updateLastPosition(position: number): void {
+    this.lastPositon = position;
   }
 
   private handleComponentPointerdown = (ev: PointerEvent): void => {
--- src/slider/View/UI/HandleView/IHandleView.ts
+++ src/slider/View/UI/HandleView/IHandleView.ts
@@ -13,6 +13,7 @@ interface IHandleView extends IEventBinder {
   calcPosition<A extends ICalcPositionArgs>(arg: A): number
   moveX(position: number): void
   moveY(position: number): void
+  getLastPosition(): number
 }
 
 export {IHandleView, ICalcPositionArgs};
