ff6f30696c18a24d6ee0baf82b0cf87fc8be3876
gamabuntat <schoshoev@yandex.ru>
2021-08-06 20:58:13 +0800

    feat(View): added view states

--- src/slider/View/View.ts
+++ src/slider/View/View.ts
@@ -1,32 +1,46 @@
+import IView from './interfaces/IView';
 import treeTemplate from './treeTemplate';
+import IViewTreeTemplate from './interfaces/IViewTreeTemplate';
 import HandleView from './UI/HandleView/HandleView';
 import {IHandleView} from './UI/HandleView/IHandleView';
-import IViewTreeTemplate from './interfaces/IViewTreeTemplate';
+import { ViewState, ViewVerticalState, ViewHorizontalState } from './ViewState';
 
-// import IResponse from '../interfaces/IResponse';
+import {IResponse} from '../helpers/IResponse';
 
 interface IComponents {
   [k: string]: HTMLElement
 }
 
-class View {
+class View implements IView {
   private static tree: IViewTreeTemplate = treeTemplate
+  private static defaultBEMBlockName = 'ui-slider'
+  private states: ViewState[]
+  private currentState: ViewState
   private components: IComponents = {}
   private blockName = ''
   private isTriggered = false
+  private handleActiveIdx = 0
   private slider: HTMLElement
   private handles: IHandleView[]
-  private handlesHandlePointermove: Array<(ev: PointerEvent) => void>
+  private handlesHandlePointerdown: Array<(ev: PointerEvent) => void>
 
-  constructor() {
+  constructor(private currentResponse: IResponse) {
+    this.states = [
+      new ViewHorizontalState(this), new ViewVerticalState(this)
+    ];
+    this.currentState = this.states[+this.currentResponse.isVertical];
     this.slider = this.createSlider(View.tree);
     this.handles = [
       new HandleView(this.components.handleStart),
       new HandleView(this.components.handleEnd)
     ];
-    this.handlesHandlePointermove = this.getHadlesHandlePointermove();
+    this.handlesHandlePointerdown = this.getHadlesHandlePointerdown();
     this.bindListeners();
     this.renderSleder();
+  }
+  
+  getCurrentResponse(): IResponse {
+    return this.currentResponse;
   }
 
   private createSlider(
@@ -35,17 +49,13 @@ class View {
   ): HTMLElement {
     const elem = document.createElement(elementType);
     this.saveComponent(
-      parent ? name : this.setBlockName(name),
+      parent ? name : this.blockName = name || View.defaultBEMBlockName,
       elem
     );
-    elem.classList.add(this.getClass(name));
+    elem.classList.add(parent ? this.getClass(name) : this.blockName);
     if (parent) { parent.insertAdjacentElement('beforeend', elem); }
     childs.forEach((node) => this.createSlider(node, elem));
     return elem;
-  }
-
-  private setBlockName(blockName: string): string {
-    return this.blockName = blockName;
   }
 
   private saveComponent(name: string, elem: HTMLElement): void {
@@ -53,7 +63,7 @@ class View {
   }
 
   private getClass(name: string): string {
-    return this.blockName + '__' + name
+    return this.blockName + (name && '__') + name
       .replace(/(?<=.)[A-Z]/g, '-$&')
       .toLowerCase()
       .replace(
@@ -62,31 +72,34 @@ class View {
       );
   }
 
-  private getHadlesHandlePointermove(): Array<(ev: PointerEvent) => void> {
+  private getHadlesHandlePointerdown(): Array<(ev: PointerEvent) => void> {
     return Array
       .from({ length: 2 })
-      .map((i, idx) => this.makeHandleHandlePointermove(idx));
+      .map((i, idx) => this.makeHandleHandlePointerdown(idx));
   }
 
-  private makeHandleHandlePointermove = (idx: number) => (ev: PointerEvent) => {
-    if (!this.isTriggered) { return; }
-    console.log('id: ' + idx);
-    console.log(ev.x);
+  private makeHandleHandlePointerdown = (idx: number) => (ev: PointerEvent) => {
+    this.isTriggered = true;
+    this.handleActiveIdx = idx;
+    this.currentState.handleHandlePointerdown();
+    console.log(this.isTriggered);
+    console.log(ev.offsetX);
   }
 
   private bindListeners(): void {
     this.handles.forEach((h, idx) => {
       h
-        .bind('pointerdown', this.handleHandlePointerdown)
-        .bind('pointermove', this.handlesHandlePointermove[idx])
+        .bind('pointerdown', this.handlesHandlePointerdown[idx])
+        .bind('pointermove', this.handleHandlePointermove)
         .bind('lostpointercapture', this.handleHandleLostpointercapture);
     });
   }
 
-  private handleHandlePointerdown = (ev: PointerEvent): void => {
-    this.isTriggered = true;
-    console.log(this.isTriggered);
-    console.log(ev.offsetX);
+  private handleHandlePointermove = (ev: PointerEvent) => {
+    if (!this.isTriggered) { return; }
+    console.log(ev.x);
+    console.log('idx: ' + this.handleActiveIdx);
+    this.currentState.handleHandlePointermove();
   }
 
   private handleHandleLostpointercapture = (): void => {
---
+++ src/slider/View/interfaces/IView.ts
@@ -0,0 +1,8 @@
+import { IResponse } from 'slider/helpers/IResponse';
+
+interface IView {
+  getCurrentResponse(): IResponse
+}
+
+export default IView;
+
