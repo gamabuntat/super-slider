b2e176962648310febd9a4cb2dc7bb9a77c89375
gamabuntat <schoshoev@yandex.ru>
2021-08-08 20:45:10 +0800

    update for deploy

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,35 @@ import View from './slider/View/View';
 
 (function ($) {
   $.fn.slider = function () {
-    console.log(
-      new View({ isVertical: false })
-    );
+    new View({ 
+      isVertical: true,
+      isInterval: true,
+      positions: [
+        {
+          max: 1,
+          min: 0
+        },
+        {
+          max: 1,
+          min: 0
+        }
+      ]
+    })
+      .parseResponse({
+        isVertical: false,
+        isInterval: true,
+        positions: [
+          {
+            max: 1,
+            min: 0
+          },
+          {
+            max: 1,
+            min: 0
+          }
+        ]
+      }) 
+    ;
     return this;
   };
 })(jQuery || {});
