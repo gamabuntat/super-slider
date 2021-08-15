149beccef3cda9e2942c5a9070386244c6782e68
gamabuntat <schoshoev@yandex.ru>
2021-08-16 02:58:39 +0800

    feat(Service): added validation

--- src/slider/Service/Service.ts
+++ src/slider/Service/Service.ts
@@ -1,12 +1,25 @@
-import IModel from 'slider/Model/IModel';
 import EventEmitter from 'slider/EventEmitter/EventEmitter';
+import IResponse from 'slider/helpers/IResponse';
+import { clamp } from 'slider/helpers/clamp';
+
+interface IValidatedOptions {
+  min?: number
+  max?: number
+  from?: number
+  to?: number
+}
+
+type TypeValidateOptionsKeys = (
+  keyof { [K in keyof IValidatedOptions]-?: IValidatedOptions[K] }
+)[];
 
 class Service extends EventEmitter {
   private static instance: Service
-  private modelDefaultOptions: TypeRequiredOptions = {
+  private readonly defaultModel: IResponse = {
+    id: '',
     min: 0,
     max: 10,
-    from: 0,
+    from: 8,
     to: 10,
     step: 1,
     isInterval: false,
@@ -14,39 +27,79 @@ class Service extends EventEmitter {
     isLabel: true,
     isScale: true,
   }
-  private models: IModel[] = []
-  private lastIndex = -1
+  private selectedModel: IResponse = this.defaultModel
+  private models: IResponse[] = []
+  private selectedIndex = -1
 
   static getInstance(): Service {
     if (!Service.instance) { Service.instance = new Service(); }
     return Service.instance; 
   }
 
-  updateModel(response: IModel): void {
+  updateModel(response: IResponse): void {
     this.addModel({ ...response });
   }
 
-  add(id = this.generateID(), o: IOptions): IModel {
-    const model: IModel = { ...this.modelDefaultOptions, ...o, id };
+  add(id = this.generateID(), o: IOptions): IResponse {
+    const model: IResponse = { 
+      ...this.selectedModel = (
+        this.models[this.findModelIndex(id)] || { ...this.defaultModel, id }
+      ),
+      ...this.getValedatedOptions(o),
+    };
     this.addModel(model);
-    this.emit({ ...model });
-    return { ...model };
+    const copy = { ...model };
+    this.emit(copy);
+    return copy;
+  }
+
+  private addModel(model: IResponse): void {
+    this.models.splice(
+      this.findModelIndex(model.id),
+      +(this.selectedIndex !== -1),
+      model
+    );
+  }
+
+  private findModelIndex(id: string): number {
+    return this.selectedIndex = this.models.findIndex((m) => m.id === id);
   }
 
   private generateID(): string {
     return String(Math.floor(Math.random() * Date.now()));
   }
 
-  private addModel(model: IModel): void {
-    this.models.splice(
-      this.findModelIndex(model.id),
-      +(this.lastIndex !== -1),
-      model
-    );
+  private getValedatedOptions(o: IOptions): IOptions {
+    const copy = { ...o };
+    const extremums: TypeValidateOptionsKeys = ['min', 'max'];
+    const positions: TypeValidateOptionsKeys = ['from', 'to'];
+    [
+      ...(!copy.min && copy.max ? extremums.reverse() : extremums),
+      ...(!copy.from && copy.to ? positions.reverse() : positions)
+    ].forEach((k) => {
+      const v = copy[k] || this.selectedModel[k];
+      copy[k] = this.validateConcreteOption(k, v, copy);
+    });
+    return copy;
   }
 
-  private findModelIndex(id: string): number {
-    return this.lastIndex = this.models.findIndex((m) => m.id === id);
+  private validateConcreteOption<K extends keyof IValidatedOptions>(
+    key: K,
+    v: number,
+    o: IOptions,
+    max = o.max ?? this.selectedModel.max,
+    min = o.min ?? this.selectedModel.min
+  ): number {
+    return {
+      min: (v: number) => Math.min(v, max),
+      max: (v: number) => Math.max(v, min),
+      from: (v: number) => clamp(min, v, Math.min(
+        o.to ?? this.selectedModel.to, max
+      )),
+      to: (v: number) => clamp(Math.max(
+        o.from ?? this.selectedModel.from, min
+      ), v, max)
+    }[key](v);
   }
 }
 
---
+++ src/slider/helpers/clamp.ts
@@ -0,0 +1,6 @@
+function clamp(min: number, v: number, max: number): number {
+  return Math.max(min, Math.min(max, v));
+}
+
+export { clamp };
+
